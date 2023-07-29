f2d38c26f678b8e52d89d362be92208bdc2c176c
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-07-29 14:56:37 +0300

    Added main structure of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="contairne">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by {""}
           <a
