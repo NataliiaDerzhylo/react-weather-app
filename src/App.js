97d66ff9cabea4fa9e2f2961ebbd9b3f139e06b8
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-07-29 14:27:52 +0300

    Added Bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://www.instagram.com/natalia_zhill/"
-          target="_blank rel=noopener"
-        >
-          {" "}
-          Nataliia Derzhylo{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/NataliiaDerzhylo/react-weather-app"
-          target="_blank rel=noopener"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="contairne">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.instagram.com/natalia_zhill/"
+            target="_blank rel=noopener"
+          >
+            {" "}
+            Nataliia Derzhylo{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NataliiaDerzhylo/react-weather-app"
+            target="_blank rel=noopener"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
