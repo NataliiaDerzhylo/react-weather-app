9f574b8f91753d9f59a611003195ec90d0b7c2be
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-07-29 10:36:48 +0300

    Debugged

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by {""}
-        <a href="https://www.instagram.com/natalia_zhill/" target="_blank">
+        <a
+          href="https://www.instagram.com/natalia_zhill/"
+          target="_blank rel=noopener"
+        >
           {" "}
           Nataliia Derzhylo{" "}
         </a>{" "}
         and is{" "}
-        <a href="#" target="_blank">
+        <a
+          href="https://www.instagram.com/natalia_zhill/"
+          target="_blank rel=noopener"
+        >
           {" "}
           open-sourced on GitHub
         </a>
