26f606c1e90bb176f3a0ccf0a770e4986050d7ef
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-07-29 10:29:55 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href="https://www.instagram.com/natalia_zhill/" target="_blank">
+          {" "}
+          Nataliia Derzhylo{" "}
+        </a>{" "}
+        and is{" "}
+        <a href="#" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
